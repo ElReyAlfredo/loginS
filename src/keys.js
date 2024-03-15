851e0dfc3e318d42fdabb6ee44838aee44a318ed
ElReyAlfredo <jdelgado7@ucol.mx>
2024-03-15 03:18:47 -0600

    ss

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   mongodb: {
     URI: "mongodb+srv://jdelgado7:mKsh4RQ6GgB7b5Yh@cluster0.4kmkl37.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    API_UIL: "http://localhost:8000",
   },
 };
